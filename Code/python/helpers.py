bf07c71e1669eca1318a9e66454003df944ea5ea
Jacob Smilg <jacob@smilg.dev>
2021-09-28 21:45:57 -0400

    add coord transformation function and load_csv

--- Code/python/helpers.py
+++ Code/python/helpers.py
@@ -2,6 +2,7 @@ import math
 from scipy.optimize import curve_fit
 from pathlib import Path
 import numpy as np
+import pandas as pd
 
 def yesno_confirm(message) -> bool:
     confirm_choice = ''
@@ -14,6 +15,13 @@ def yesno_confirm(message) -> bool:
 def map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+def sph2cart(az, el, r):
+    rcos_theta = r * np.cos(el)
+    x = rcos_theta * np.cos(az)
+    y = rcos_theta * np.sin(az)
+    z = r * np.sin(el)
+    return x, y, z
+
 def exp_function(x, a, b, c, d):
     return a*np.exp(b*x) + c*np.exp(d*x)
 
@@ -24,11 +32,20 @@ def save_csv(data):
             print('invalid path! make sure the directory exists and is accessible.')
         elif data_dir.is_file():
             if yesno_confirm('a file already exists at that location. would you like to replace it?'):
-                data.to_csv(data_dir)
+                data.to_csv(data_dir, index=False)
                 break
         else:
-            data.to_csv(data_dir)
+            data.to_csv(data_dir, index=False)
             break
+
+def load_csv():
+    while True:
+        data_dir = Path(input('enter a file path ending in .csv for your data: '))
+        if data_dir.is_file():
+            data = pd.read_csv(data_dir)
+            return data
+        else:
+            print('the specified file was not found!')
 
 def fit_data(data, xkey, ykey):
     popt, pcov = curve_fit(exp_function, data[xkey], data[ykey], method='trf')
